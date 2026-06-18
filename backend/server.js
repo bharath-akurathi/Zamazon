e6f03405e997f03ae510afb22b6a749a248a8b7b
bharathakurathi436 <139782092+bharath-akurathi@users.noreply.github.com>
2026-06-18 15:45:11 +0530

    update route handling for production

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,8 @@ app.get('/health', (req, res) => {
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/dist')));
-  app.get('*', (req, res) => {
+  // Change '*' to '/{*splat}'
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
   });
 }
